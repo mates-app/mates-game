e58a9543b7941c4242a6bbac25bdaaa70625f89f
josecullen <jose_cullen@hotmail.com>
2016-11-13 13:27:04 -0300

    Se actualiza a última versión de angular

--- app/main.ts
+++ app/main.ts
@@ -1,3 +1,4 @@
+/// <reference path="../typings/globals/hammerjs/index.d.ts" />
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
 platformBrowserDynamic().bootstrapModule(AppModule);
