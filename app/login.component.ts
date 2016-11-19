465dd3c3f25ec4a71f818c077c43f0a9965448b1
José Cullen <josecullen@Joses-MacBook-Pro.local>
2016-11-19 15:19:55 -0300

    fixed #1 - Se agrega formulario de login

--- app/login.component.ts
+++ app/login.component.ts
@@ -2,66 +2,159 @@ import { Component }   from '@angular/core';
 import { Router }      from '@angular/router';
 import { AuthService } from './auth.service';
 import { User } from './models'
+import {MdIconRegistry} from "@angular/material";
 
 
 @Component({
   template: `
-    <h2>LOGIN</h2>
-    <p>{{message}}</p>
-    <input type="text" [(ngModel)]="user.username">
+<div class="centered-container">
+    <md-card>
+      <md-card-subtitle></md-card-subtitle>
+      <md-card-title>Ingreso a Mates</md-card-title>   
+        <md-card-content>
+          
 
-    <p>
-      <button (click)="login()"  *ngIf="!authService.isLoggedIn">Login</button>
-      <button (click)="signUp()">SignUp</button>
+          <md-tab-group (selectChange)="onTabChange($event)">
+            <md-tab>
+              <template md-tab-label>Ingresá</template>
+              <template md-tab-content>
+                <md-input 
+                  placeholder="usuario" maxlength="20" 
+                  class="full-width"    required
+                  [(ngModel)]="user.username"
+                  (change)="userValid()">
+                  <span md-suffix>
+                    <md-icon *ngIf="usernameValid" style="color:#AED581;">check_circle</md-icon>
+                    <md-icon *ngIf="!usernameValid && usernameValid != undefined" style="color:#921919;">error</md-icon>
+                  </span>
+                  <md-hint align="end" style="color:#921919;">{{messages.userMessage}}</md-hint>
+                </md-input>
+                <br>
+                <md-input 
+                  placeholder="password" maxlength="20" 
+                  type="password" class="full-width"
+                  required
+                  [(ngModel)]="user.password">
+                </md-input>
+              </template>
+            </md-tab>
+            <md-tab>
+              <template md-tab-label>Registrate</template>
+              <template md-tab-content>
+                <md-input 
+                  placeholder="usuario" maxlength="20" 
+                  class="full-width"    required
+                  [(ngModel)]="user.username"
+                  (change)="userValid()">
+                  <span md-suffix>
+                    <md-icon *ngIf="usernameValid" style="color:#AED581;">check_circle</md-icon>
+                    <md-icon *ngIf="!usernameValid" style="color:#921919;">error</md-icon>
+                  </span>
+                  <md-hint align="end" style="color:#921919;">{{messages.userMessage}}</md-hint>
+                </md-input>
+                <br>
+                <md-input 
+                  placeholder="password" maxlength="20" 
+                  type="password" class="full-width"
+                  required
+                  [(ngModel)]="user.password">
+                </md-input>
+              </template>
+            </md-tab>
+          </md-tab-group>
 
-      <button (click)="logout()" *ngIf="authService.isLoggedIn">Logout</button>
-    </p>
-    
-    `
+          
+        </md-card-content>
+        <md-card-actions style="text-align:center">
+          <button 
+            md-raised-button 
+            class="width-90"
+            (click)="onSubmit()"
+            [disabled]="!formIsValid()">ENTRAR</button>
+        </md-card-actions>
+    </md-card>
+</div>
+    `,
+    styleUrls: [ 'styles.css' ],
+    viewProviders: [MdIconRegistry]
 })
 export class LoginComponent {
   user:User = new User()
-
-  message: string;
-  
-  constructor(public authService: AuthService, public router: Router) {
-    this.setMessage();
-  }
-  
-  setMessage() {
-    this.message = 'Logged ' + (this.authService.isLoggedIn ? 'in' : 'out');
+  tabIndex:number = 0
+  usernameValid:boolean = undefined
+  messages:any = {
+    userMessage: "",
+    passMessage: ""
   }
 
+  constructor(
+    public  authService   : AuthService, 
+    public  router        : Router,
+    private mdIconRegistry:MdIconRegistry) {
+      
+    mdIconRegistry
+      .addSvgIcon('thumb-up', '/game-mates/icon/assets/thumbup-icon.svg')
+      .addSvgIconSetInNamespace('core', '/game-mates/icon/assets/core-icon-set.svg')
+      .registerFontClassAlias('fontawesome', 'fa');
+  }
+  
+  formIsValid(){
+    return this.usernameValid
+        && this.user.password.length >= 8
+        && this.user.password.length <= 20
+  }
+
+  onSubmit(){
+    this.tabIndex === 0 ? this.login() : this.signUp()
+  }
+
+  onTabChange(tab){
+    this.tabIndex = tab.index
+  }
+  
+ 
   signUp(){
-    this.authService.signup(this.user).subscribe(
-      isSignUpOK => {
-        if(isSignUpOK){
-          this.login()
-        }else{
-          console.log('signup error')
-        }
-      }
-    )
+    this.authService
+        .signup(this.user)
+        .subscribe(
+          isSignUpOK => isSignUpOK ? this.login() : console.log('signup error')      
+        )
   }
   
   login() {
-    this.message = 'Trying to log in ...';
     this.authService.login(this.user).subscribe(isValid => {
-      this.setMessage();
-      console.log('isValid',isValid)
       if (this.authService.isLoggedIn) {
-        // Get the redirect URL from our auth service
-        // If no redirect has been set, use the default
         let redirect = this.authService.redirectUrl ? this.authService.redirectUrl : '/menu';
-        // Redirect the user
         this.router.navigate([redirect]);
       }
     });
   }
   
-  logout() {
-    this.authService.logout();
-    this.setMessage();
+  private userValid():boolean{
+    let result:boolean = 
+    this.user.username.length >= 8
+    && this.user.username.length <= 20
+
+
+    if(result) 
+      this.authService.exists(this.user.username).subscribe(res => {
+        console.log(res)
+        this.tabIndex === 0 
+          ? result = res
+          : result = !res
+
+        if(!result)
+          this.tabIndex === 0 
+            ? this.messages.userMessage = "El usuario no existe"
+            : this.messages.userMessage = "El usuario ya existe"
+        else this.messages.userMessage = "" 
+        this.usernameValid = result
+      })
+    else 
+      this.messages.userMessage = "El usuario debe tener entre 8 y 20 caracteres"      
+    
+    this.usernameValid = result
+    return result
   }
 
 }
